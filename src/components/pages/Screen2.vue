a528abc793556e64bebea127a7fdef7aaaf17060
NUCAL <selin.yuksel@nucal.com>
2023-03-28 16:05:15 +0300

    jkljllkjhl

---
+++ src/components/pages/Screen2.vue
@@ -0,0 +1,111 @@
+<template>
+  <DropZone ncBgType="bgColor" backgroundImage="" id="a7045d0d-a1e3-4c99-81f5-9f0cf55a2e6c"
+    v-bind:style="{'width':'100%','height':'100%'}">
+    <NucGroupElement v-bind:isLock="false" bgColor="" ncBgType="bgColor" position="relative" ncTooltipText=""
+      v-bind:snapX="false" v-bind:snapY="false" id="5a3cf711-b446-4a4c-8ccf-852f392d3af4"
+      v-bind:style="{'color':'#FFFFFF','width':'100%','height':'100%','zIndex':2,'tabIndex':'-1','boxShadow':'','fontStyle':'normal','marginTop':'0px','textAlign':'center','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','lineHeight':'1.25','marginLeft':'0px','paddingTop':'','textShadow':'','visibility':'visible','borderColor':'#767676','borderStyle':'solid','marginRight':'0px','paddingLeft':'','marginBottom':'0px','paddingRight':'','paddingBottom':'','borderTopWidth':'1px','textDecoration':'none','borderLeftWidth':'1px','borderRightWidth':'1px','borderBottomWidth':'1px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}"/>
+    <NucDateTimePicker i18n="en" theme="theme--light01" v-bind:value="1678914000000" format="DD.MM.YYYY HH:mm"
+      v-bind:visible="true" position="relative" clockType="digital" dataModel="datetime" v-bind:isEnabled="true"
+      pickerType="datetime" v-bind:displayMode="false" placeholder="" calendarType="date" v-bind:disableDates="[]"
+      ncTypography="body2" ncTooltipText="" firstDayOfWeek="1"
+      v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':true,'structure':[{'type':'datetime','update':{'type':'dataModel'},'propName':'value','displayName':'Value'}]}"
+      v-bind:disableDateRange="[]" id="a2559dfb-b7d8-49aa-9f7c-68906ff90af3"
+      v-bind:style="{'color':'black','width':'100%','height':'100%','zIndex':3,'fontStyle':'none','marginTop':'0px','textAlign':'left','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','marginLeft':'0px','paddingTop':'','visibility':'visible','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'','paddingBottom':'0px','textDecoration':'none'}"/>
+  </DropZone>
+</template>
+<script>
+export default {
+  name: 'Screen2',
+  data() {
+    return {};
+  },
+  props: {},
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@media (min-width: 1024.05px){[id='a7045d0d-a1e3-4c99-81f5-9f0cf55a2e6c'] {
+             display: grid; 
+             grid-template-columns: 28.359375rem 27.682290077209473rem 12.5rem 0.8333349227905273rem 30.625rem;
+             grid-template-rows: minmax(3.515625rem, max-content) minmax(19.869791269302368rem, max-content) minmax(2.34375rem, max-content) minmax(1.2239587306976318rem, max-content) minmax(33.046875rem, max-content);
+          }
+[id='5a3cf711-b446-4a4c-8ccf-852f392d3af4']{
+          grid-area: 2/2/5/5;
+          
+          
+          }
+[id='a2559dfb-b7d8-49aa-9f7c-68906ff90af3']{
+          grid-area: 3/3/4/4;
+          font-size: Max(Min(14rem, 12px),1.09375rem);
+          
+          }}
+
+@media (min-width: 768.05px) and (max-width: 1024px){[id='a7045d0d-a1e3-4c99-81f5-9f0cf55a2e6c'] {
+             display: grid; 
+             grid-template-columns: 24.365234375rem 34.60286259651184rem 15.625rem 1.0416686534881592rem 24.365234375rem;
+             grid-template-rows: minmax(4.39453125rem, max-content) minmax(24.83723908662796rem, max-content) minmax(2.9296875rem, max-content) minmax(1.5299484133720398rem, max-content) minmax(3.125rem, max-content);
+          }
+[id='5a3cf711-b446-4a4c-8ccf-852f392d3af4']{
+          grid-area: 2/2/5/5;
+          
+          
+          }
+[id='a2559dfb-b7d8-49aa-9f7c-68906ff90af3']{
+          grid-area: 3/3/4/4;
+          font-size: Max(Min(14rem, 12px),1.3671875rem);
+          
+          }}
+
+@media (min-width: 480.05px) and (max-width: 768px){[id='a7045d0d-a1e3-4c99-81f5-9f0cf55a2e6c'] {
+             display: grid; 
+             grid-template-columns: 4.166666667000001rem 61.867721314964236rem 27.936507936304757rem 1.8624374147309983rem 4.166666667000001rem;
+             grid-template-rows: minmax(5.859375rem, max-content) minmax(44.40740651900678rem, max-content) minmax(5.238095238057142rem, max-content) minmax(2.735450623507511rem, max-content) minmax(4.166666667rem, max-content);
+          }
+[id='5a3cf711-b446-4a4c-8ccf-852f392d3af4']{
+          grid-area: 2/2/5/5;
+          
+          
+          }
+[id='a2559dfb-b7d8-49aa-9f7c-68906ff90af3']{
+          grid-area: 3/3/4/4;
+          font-size: Max(Min(14rem, 12px),2.4444444444266664rem);
+          
+          }}
+
+@media (min-width: 375.05px) and (max-width: 480px){[id='a7045d0d-a1e3-4c99-81f5-9f0cf55a2e6c'] {
+             display: grid; 
+             grid-template-columns: 5.000000000000007rem 60.74285365513392rem 27.42857142857143rem 1.828574916294643rem 5.000000000000007rem;
+             grid-template-rows: minmax(9.375rem, max-content) minmax(43.5999991280692rem, max-content) minmax(5.142857142857142rem, max-content) minmax(2.685715157645089rem, max-content) minmax(5rem, max-content);
+          }
+[id='5a3cf711-b446-4a4c-8ccf-852f392d3af4']{
+          grid-area: 2/2/5/5;
+          
+          
+          }
+[id='a2559dfb-b7d8-49aa-9f7c-68906ff90af3']{
+          grid-area: 3/3/4/4;
+          font-size: Max(Min(14rem, 12px),2.4rem);
+          
+          }}
+
+@media (min-width: 0px) and (max-width: 375px){[id='a7045d0d-a1e3-4c99-81f5-9f0cf55a2e6c'] {
+             display: grid; 
+             grid-template-columns: 6.4rem 58.8530759858631rem 26.575238095238095rem 1.7716859188988097rem 6.4rem;
+             grid-template-rows: minmax(12rem, max-content) minmax(42.24355471075149rem, max-content) minmax(4.982857142857143rem, max-content) minmax(2.6021595749627977rem, max-content) minmax(6.4rem, max-content);
+          }
+[id='5a3cf711-b446-4a4c-8ccf-852f392d3af4']{
+          grid-area: 2/2/5/5;
+          
+          
+          }
+[id='a2559dfb-b7d8-49aa-9f7c-68906ff90af3']{
+          grid-area: 3/3/4/4;
+          font-size: Max(Min(14rem, 12px),2.3253333333333335rem);
+          
+          }}
+</style>
